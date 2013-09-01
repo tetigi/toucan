7b33fae7deb16a69a46157a095b4734cb0a1241e
tetigi <luke.tomlin@gmail.com>
2013-09-01 11:09:21 +0100

    Adding tests. Playing around.

---
+++ java/src/frontend/parser/ParserCupTest.java
@@ -0,0 +1,22 @@
+package frontend.parser;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParserCupTest {
+
+  ParserCup parser;
+
+  @Before
+  public void setUp() throws Exception {
+    parser = new ParserCup();
+  }
+
+  @Test
+  public void testParse() {
+    fail("Not yet implemented");
+  }
+
+}
